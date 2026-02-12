da6963b4c0c9c39cbb70df26dc861e7643c0e1df
Jonas Svirkær <jonassvirkaer@gmail.com>
2026-02-12 09:33:04 +0100

    NY

--- mos score modller/Linært.py
+++ mos score modller/Linært.py
@@ -2,7 +2,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 import os
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split, cross_val_score
